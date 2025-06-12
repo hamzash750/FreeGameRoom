0b7ec716e40a52ae251b9048166415e3ffad2c67
hamzashafiq <hamzash75@gmail.com>
2025-06-12 23:29:08 +0400

    working game

--- game-portal/src/app/pages/game-list/game-list.ts
+++ game-portal/src/app/pages/game-list/game-list.ts
@@ -10,10 +10,27 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, RouterModule],
 })
 export class GameList {
+  currentYear = new Date().getFullYear();
 games = [
-  { route: '/tic-tac-toe', name: 'Tic Tac Toe', description: 'Classic tic tac toe game' },
-  { route: '/snake', name: 'Snake', description: 'Grow the snake and avoid walls!' },
-  { route: '/memory', name: 'Memory Game', description: 'Match the cards!' },
+  {
+    route: '/tic-tac-toe',
+    name: 'Tic Tac Toe',
+    description: 'Classic tic tac toe game',
+    icon: 'https://img.icons8.com/ios-filled/200/000000/tic-tac-toe.png'
+  },
+  {
+    route: '/snake',
+    name: 'Snake',
+    description: 'Grow the snake and avoid walls!',
+    icon: 'https://img.icons8.com/ios-filled/200/000000/snake.png'
+  },
+  {
+    route: '/memory',
+    name: 'Memory Game',
+    description: 'Match the cards!',
+   icon: 'https://cdn-icons-png.flaticon.com/512/3050/3050525.png'
+  }
 ];
+
 
 }
